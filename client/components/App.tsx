d7d66f96937962c638c2e47b9568a980abf7e4b6
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-08-04 14:13:16 +1200

    Create base Nav component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,5 @@
 import { useFruits } from '../hooks/useFruits.ts'
+import Nav from './Nav.tsx'
 
 function App() {
   const { data } = useFruits()
@@ -7,6 +8,7 @@ function App() {
     <>
       <div className="app">
         <h1>PokeDB</h1>
+        <Nav />
         <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
       </div>
     </>
---
+++ client/components/Nav.tsx
@@ -0,0 +1,12 @@
+export default function Nav() {
+  return (
+    <>
+      <div>
+        <ul>
+          <li>Pokemon Database</li>
+          <li>IV Calculator</li>
+        </ul>
+      </div>
+    </>
+  )
+}
