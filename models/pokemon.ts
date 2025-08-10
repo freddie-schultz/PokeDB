fbc547ec3681f1a34b5340017d294cfb6425e474
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-08-10 14:02:05 +1200

    Add Pokemon.ts model file

---
+++ models/pokemon.ts
@@ -0,0 +1,13 @@
+export interface Pokemon {
+  id: number
+  species: number
+  nickname: number
+  level: number
+  nature: string
+  ivHp: number
+  ivAttack: number
+  ivDefense: number
+  ivSpecialAttack: number
+  ivSpecialDefense: number
+  ivSpeed: number
+}
