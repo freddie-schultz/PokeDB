5806a9973a3a4c7828a68e4ccf8d0bdfca38c6fe
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-08-10 14:05:38 +1200

    Add database function to return all pokemon

--- server/db/functions/pokemon.ts
+++ server/db/functions/pokemon.ts
@@ -1,1 +1,8 @@
-export async function getAllPokemon() {}
+import { Pokemon } from '../../../models/pokemon'
+import database from '../connection.ts'
+
+export async function getAllPokemon(): Promise<Pokemon[]> {
+  const results = await database('pokemon').select()
+
+  return results
+}
