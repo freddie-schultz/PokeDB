ba09293074a8313b408d0bc76c17dd94d7a5ef7e
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-08-11 17:40:08 +1200

    Update PokemonTile component to display all pokemon details

--- client/components/PokemonTile.tsx
+++ client/components/PokemonTile.tsx
@@ -5,7 +5,16 @@ export default function PokemonTile(props: Pokemon) {
     <>
       <div className="pokemon-tile">
         <h2>{props.nickname}</h2>
-        <h3>{props.species}</h3>
+        <h3>{`(${props.species})`}</h3>
+        <h2>Level: {props.level}</h2>
+        <h2>Nature: {props.nature}</h2>
+        <h3>IVs:</h3>
+        <p>HP: {props.ivHp}</p>
+        <p>Attack: {props.ivAttack}</p>
+        <p>Defense: {props.ivDefense}</p>
+        <p>Special Attack: {props.ivSpecialAttack}</p>
+        <p>Special Defense: {props.ivSpecialDefense}</p>
+        <p>Speed: {props.ivSpeed}</p>
       </div>
     </>
   )
